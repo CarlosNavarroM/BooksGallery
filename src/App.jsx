2f94184d5c018a73a060184b53770e52ad560f35
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-04 05:01:31 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Library from './assets/components/Library';
 import BookDetails from './assets/components/BookDetails';
 import './styles/App.css';
+
+
+
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Libros de Programación</h1>
+          <h1><Link to="/">Libros de Programación</Link></h1>
         </header>
         <Routes>
           <Route path="/" element={<Library />} />
