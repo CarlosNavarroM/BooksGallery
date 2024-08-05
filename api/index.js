025fda3254254df576e268b67c996e05c52a0ad8
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-04 21:01:38 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
 const app = express();
 const port = process.env.PORT || 3001;
