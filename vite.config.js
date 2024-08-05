d95f40e0c038482593dfdeb4a334e960f3540749
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-05 01:11:11 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,12 @@ dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 });
