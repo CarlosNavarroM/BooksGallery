d86b44f69f6a22ae14e474d3d42360514d8bc3b2
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-06 02:00:46 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Cargar variables de entorno
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const envVariables = loadEnv(mode, process.cwd());
-  const apiUrl = envVariables.VITE_API_URL || 'https://books-gallery-api.vercel.app';
+  // Cargar variables de entorno desde .env
+  const env = loadEnv(mode, process.cwd(), ''); 
+  const apiUrl = env.VITE_API_URL; // Accede a la variable directamente
 
   return {
     plugins: [react()],
@@ -13,8 +12,9 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': {
           target: apiUrl,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true, // Necesario para evitar errores CORS
+          rewrite: (path) => path.replace(/^\/api/, ''), // Reescribir la ruta para que coincida con tu API
+          secure: false, // Solo si tu API NO usa HTTPS
         },
       },
     },
