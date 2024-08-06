d2052f4a47cf8c829a3295019ce551c57109bd04
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-06 02:36:21 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,36 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-export default defineConfig(({ mode }) => {
-  // Cargar variables de entorno
-  const env = loadEnv(mode, process.cwd(), '');
-  const apiUrl = env.VITE_API_URL || 'https://books-gallery-api.vercel.app';
+dotenv.config();
 
-  return {
-    plugins: [react()],
-    root: './client',
-    server: {
-      proxy: { // El proxy se mantiene para desarrollo local
-        '/api': {
-          target: apiUrl,
-          changeOrigin: true,
-        },
+const apiURL = process.env.NODE_ENV === 'production' 
+  ? process.env.VITE_API_URL 
+  : process.env.VITE_API_URL_DEV;
+
+export default defineConfig({
+  plugins: [react()],
+  root: './client',
+  server: {
+    proxy: {
+      '/api': {
+        target: apiURL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
-    build: {
-      outDir: '../dist',
-      rollupOptions: {
-        input: {
-          main: './client/src/main.jsx' 
-        }
+  },
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: './client/src/main.jsx' // Corrige la ruta de entrada principal
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-  };
+  },
 });
