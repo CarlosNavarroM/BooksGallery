fddf2c618bb65e0782c0940d8ce6ab5cbd65b141
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-06 01:52:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  // Cargar variables de entorno
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const envVariables = loadEnv(mode, process.cwd());
+  const apiUrl = envVariables.VITE_API_URL || 'https://books-gallery-api.vercel.app';
 
-export default defineConfig({
-  plugins: [react()],
-  root: './client', // La carpeta 'client' es la raíz para Vite
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Asegura que el proxy apunta a tu backend
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  build: {
-    outDir: '../dist', // Los archivos de construcción se colocarán fuera de la carpeta 'client'
-    rollupOptions: {
-      input: {
-        main: '/src/main.jsx' // Establece 'main.jsx' como el archivo de entrada principal
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Facilita las importaciones desde la carpeta 'src'
-    },
-  },
+  };
 });
