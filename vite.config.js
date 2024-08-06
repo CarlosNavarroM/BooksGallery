078663a68a8637c78bd96276ec8eb200057646fc
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-06 02:14:41 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   // Cargar variables de entorno desde .env
   const env = loadEnv(mode, process.cwd(), '');
-  const apiUrl = env.VITE_API_URL || 'https://books-gallery-api.vercel.app'; // URL por defecto en caso de no encontrar la variable
-
-  // Determinar si estamos en desarrollo o producción
-  const isDevelopment = mode === 'development';
+  const apiUrl = env.VITE_API_URL || 'https://books-gallery-api.vercel.app'; // URL de tu API en Vercel
 
   return {
     plugins: [react()],
-    base: isDevelopment ? '/' : '/books-gallery-zeta/', // Base para producción (si es diferente)
+    base: '/',  // Ajusta esto si tu aplicación está en un subdirectorio en Vercel
     server: {
-      proxy: isDevelopment ? {  // Proxy solo en desarrollo
+      proxy: {
         '/api': {
-          target: apiUrl,
+          target: apiUrl, 
           changeOrigin: true, 
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          secure: false, // Solo si tu API NO usa HTTPS
+          rewrite: (path) => path.replace(/^\/api/, ''), 
+          secure: true, // Asumiendo que tu API usa HTTPS
         },
-      } : {}, // No hay proxy en producción
-    },
-    build: {
-      outDir: 'dist', // Directorio de salida para la compilación de producción
+      },
     },
   };
 });
