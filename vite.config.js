5dfceeaddd61a1b782154ba96b688831e1c75bcb
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-06 02:26:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,41 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  // Cargar variables de entorno desde .env
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiUrl = env.VITE_API_URL || 'https://books-gallery-api.vercel.app'; 
 
-export default defineConfig({
-  plugins: [react()],
-  root: './client',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://books-gallery-api.vercel.app/',
-        changeOrigin: true,
+  // Determinar si estamos en desarrollo o producción
+  const isDevelopment = mode === 'development';
+
+  return {
+    plugins: [react()],
+    root: path.resolve(__dirname, 'client'), // Ruta absoluta al directorio 'client'
+    server: {
+      proxy: isDevelopment ? {  // Proxy solo en desarrollo
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true, 
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: true, // Asumiendo que tu API usa HTTPS
+        },
+      } : {}, // No usar proxy en producción
+    },
+    build: {
+      outDir: '../dist', 
+      emptyOutDir: true, // Limpiar el directorio de salida antes de construir
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'client/src/main.jsx'), // Ruta absoluta al archivo de entrada principal
+        },
       },
     },
-  },
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: {
-        main: './client/src/main.jsx' // Corrige la ruta de entrada principal
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'client/src'), // Alias para 'src'
+      },
     },
-  },
+  };
 });
